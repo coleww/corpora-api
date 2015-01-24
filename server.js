9fa339ad0a5d7a877bf7c3b8757b990f61e81043
Cole Willsea <coleww@gmail.com>
2015-01-24 10:14:20 -0800

    removes unneeded data path var

--- server.js
+++ server.js
@@ -22,14 +22,13 @@ var existsInIndex = function(path, index){
 module.exports = function(){
   return http.createServer(function (req, res) {
     var uri = url.parse(req.url).pathname.replace(".json", "").substr(1);
-    var dataPath = "corpora/data/" + uri + ".json";
     var index = buildIndex();
 
     if (uri === "") {
       res.writeHead(200, { "Content-Type": "application/json" });
       res.end(JSON.stringify({description: "an API for the corpora", data: index}));
     } else if (existsInIndex(uri, index)) {
-      fs.readFile(dataPath, function(err, data){
+      fs.readFile("corpora/data/" + uri + ".json", function(err, data){
         res.writeHead(200, { "Content-Type": "application/json" });
         res.end(JSON.stringify({data: JSON.parse(data)}));
       });
