7ee6df81711893e9f0780459eb407ad8f7bb988a
Cole Willsea <coleww@gmail.com>
2015-01-24 09:42:11 -0800

    adds function for pre-building data index to server

--- server.js
+++ server.js
@@ -2,13 +2,25 @@ var http = require('http');
 var fs = require('fs');
 var url = require("url");
 
+
+var buildIndex = function(){
+  var index = {};
+  fs.readdirSync("corpora/data").forEach(function(dir){
+    index[dir] = fs.readdirSync("corpora/data/" + dir).map(function(el){
+      return el.replace(".json", "");
+    });
+  });
+  return index;
+};
+
 module.exports = function(){
   return http.createServer(function (req, res) {
     var uri = url.parse(req.url).pathname;
     var dataPath = "corpora/data" + uri + ".json";
+    var index = buildIndex();
     if (uri === "/") {
       res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end();
+      res.end(JSON.stringify({description: "an API for the corpora", data: index}));
 
     } else if (fs.existsSync(dataPath)) {
       res.writeHead(200, { 'Content-Type': 'application/json' });
