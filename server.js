22859942d8c0fea4cb4e1f43e9ca32f48fa776e8
Cole Willsea <coleww@gmail.com>
2015-01-24 09:50:21 -0800

    handling the 404 case

--- server.js
+++ server.js
@@ -21,13 +21,11 @@ module.exports = function(){
     if (uri === "/") {
       res.writeHead(200, { 'Content-Type': 'application/json' });
       res.end(JSON.stringify({description: "an API for the corpora", data: index}));
-
     } else if (fs.existsSync(dataPath)) {
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end();
+
     } else {
-      res.writeHead(404, {"Content-Type": "text/plain"});
-      res.end("404 Not Found\n");
+      res.writeHead(404, {"Content-Type": "application/json"});
+      res.end(JSON.stringify({error: "404 Not Found"}));
       // 404
     }
 
--- test.js
+++ test.js
@@ -14,6 +14,21 @@ test.get("/")
     if (res.body.data.foods.indexOf("fruits") === -1) throw "Needs data in that data";
   });
 
-// test.get("/bork");
+test.get("/bork")
+  .expect('Content-Type', /json/)
+  .expect(404)
+  .end(function(err, res){
+    if (err) throw err;
+    if (res.body.error !== "404 Not Found") throw "Needs an error description";
+  });
 
-// test.get("/food/fruits");
+
+test.get("/foods/fruits")
+  .expect('Content-Type', /json/)
+  .expect(200)
+  .end(function(err, res){
+    if (err) throw err;
+    // if (!res.body.description) throw "Needs a description";
+    // if (!res.body.data) throw "Needs some data";
+    // if (res.body.data.foods.indexOf("fruits") === -1) throw "Needs data in that data";
+  });
