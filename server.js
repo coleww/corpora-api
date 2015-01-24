2f025d4b2ec3b7c784df065f3428028b9d41dc5f
Cole Willsea <coleww@gmail.com>
2015-01-24 09:56:55 -0800

    formatting

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var http = require('http');
-var fs = require('fs');
+var http = require("http");
+var fs = require("fs");
 var url = require("url");
 
 
@@ -16,19 +16,19 @@ var buildIndex = function(){
 module.exports = function(){
   return http.createServer(function (req, res) {
     var uri = url.parse(req.url).pathname.replace(".json", "");
-
     var dataPath = "corpora/data" + uri + ".json";
     var index = buildIndex();
+
     if (uri === "/") {
-      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.writeHead(200, { "Content-Type": "application/json" });
       res.end(JSON.stringify({description: "an API for the corpora", data: index}));
     } else if (fs.existsSync(dataPath)) {
       fs.readFile(dataPath, function(err, data){
-        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.writeHead(200, { "Content-Type": "application/json" });
         res.end(JSON.stringify({data: JSON.parse(data)}));
       });
     } else {
-      res.writeHead(404, {"Content-Type": "application/json"});
+      res.writeHead(404, { "Content-Type": "application/json" });
       res.end(JSON.stringify({error: "404 Not Found"}));
     }
   });
