f0012a0b507f40c2357fa021b0359d09e42c4bc9
Cole Willsea <coleww@gmail.com>
2015-01-24 10:12:41 -0800

    checks file presence against index and ditches leading slash on path

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var url = require("url");
 
 var buildIndex = function(){
   var index = {};
-  fs.readdirSync("corpora/data").forEach(function(dir){
+  fs.readdirSync("corpora/data/").forEach(function(dir){
     index[dir] = fs.readdirSync("corpora/data/" + dir).map(function(el){
       return el.replace(".json", "");
     });
@@ -13,16 +13,22 @@ var buildIndex = function(){
   return index;
 };
 
+var existsInIndex = function(path, index){
+  var parts = path.split("/");
+  return index[parts[0]] && index[parts[0]].indexOf(parts[1]) !== -1;
+};
+
+
 module.exports = function(){
   return http.createServer(function (req, res) {
-    var uri = url.parse(req.url).pathname.replace(".json", "");
-    var dataPath = "corpora/data" + uri + ".json";
+    var uri = url.parse(req.url).pathname.replace(".json", "").substr(1);
+    var dataPath = "corpora/data/" + uri + ".json";
     var index = buildIndex();
 
-    if (uri === "/") {
+    if (uri === "") {
       res.writeHead(200, { "Content-Type": "application/json" });
       res.end(JSON.stringify({description: "an API for the corpora", data: index}));
-    } else if (fs.existsSync(dataPath)) {
+    } else if (existsInIndex(uri, index)) {
       fs.readFile(dataPath, function(err, data){
         res.writeHead(200, { "Content-Type": "application/json" });
         res.end(JSON.stringify({data: JSON.parse(data)}));
